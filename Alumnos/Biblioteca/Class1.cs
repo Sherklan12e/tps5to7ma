19bc0b51c658e152d10cf099e7c6e18316e7ba78
Sherklan12e <davismendoza.08@gmail.com>
2024-10-03 19:12:05 -0300

    j

---
+++ Alumnos/Biblioteca/Class1.cs
@@ -0,0 +1,14 @@
+﻿// namespace Biblioteca;
+
+// public class Class1 
+// {
+
+// }
+
+// Una concesionaria de vehículos desea una aplicación para administrar las ventas de sus productos y clasificarlos por tipo. Todos los autos tienen el número de serie del motor, marca, color, país de fabricación, año y precio. De todos los modelos de vehículos que se comercializan tienen variantes a combustión o eléctricos.
+
+// Los vehículos se clasifican en autos compactos, autos de lujo, deportivos, camionetas y familiar, etc. Para los autos familiares, también es importante almacenar la cantidad de pasajeros; mientras que para las camionetas se debe controlar la capacidad de carga en kgs. y la cantidad de ejes. Para los autos eléctricos se conoce la dimensión de carga de la batería (tiempo de hora) y para los autos a combustión el volumen del tanque (carga de combustible).
+
+// Se debe poder consultar el kilometraje y el consumo de cada vehículo. Si el vehículo es de combustión, si es un auto su consumo es de 1 litro por cada 13 km recorridos. Para las camionetas el consumo por litro es 10 km. En el caso de los vehículos eléctricos el consumo por kilómetro es de 20 kWh aproximadamente.
+
+// La aplicación debe informar el consumo del vehículo en base a una cantidad de kilómetros recorridos y su tipo (si es de combustión o eléctrico).
